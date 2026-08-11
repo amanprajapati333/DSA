34643823913a2c038156a25e3f7232f4a3ab5608
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-08-12 02:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (70.23%) - LeetSync

---
+++ LeetcodeProblems/941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+
+            if (nums[low] % 2 == 0) {
+                low++;
+            } 
+            else if (nums[high] % 2 == 1) {
+                high--;
+            } 
+            else {
+                int temp = nums[low];
+                nums[low] = nums[high];
+                nums[high] = temp;
+
+                low++;
+                high--;
+            }
+        }
+
+        return nums;
+    }
+}
