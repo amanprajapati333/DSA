e761fadebda19cf253edb52cab8c4e009ad5f25b
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-08-20 10:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (91.06%) - LeetSync

---
+++ LeetcodeProblems/27-remove-element/remove-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            if (nums[low] == val) {
+                nums[low] = nums[high];
+                high--;
+            } else {
+                low++;
+            }
+        }
+
+        return high + 1;
+    }
+}
