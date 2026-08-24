956e273575db9cc9a9988817e7ed467ce05acc91
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-08-25 02:03:59 +0530

    Time: 3 ms (98.38%) | Memory: 46.8 MB (17.45%) - LeetSync

--- LeetcodeProblems/2886-faulty-keyboard/faulty-keyboard.java
+++ LeetcodeProblems/2886-faulty-keyboard/faulty-keyboard.java
@@ -1,5 +1,23 @@
 class Solution {
     public String finalString(String s) {
+     
+        StringBuilder sb= new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+
+            if(ch!='i'){
+                sb.append(ch);
+            }else{
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
+        /*
         ArrayList<Character> list = new ArrayList<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -17,5 +35,7 @@ class Solution {
         }
 
         return result.toString();
+       
     }
 }
+ */
