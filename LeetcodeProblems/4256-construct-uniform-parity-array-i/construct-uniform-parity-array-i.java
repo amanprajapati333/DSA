2faedb94db9618029b5662a6478b99da0ed21704
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-03 01:19:37 +0530

    Time: 1 ms (32.15%) | Memory: 45.1 MB (73.96%) - LeetSync

---
+++ LeetcodeProblems/4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int n = nums1.length;
+        int[] nums2 = new int[n];
+
+        for(int i = 0; i < n; i++){
+            if(nums1[i] % 2 == 0)
+                nums2[i] = nums1[i];
+            else
+                nums2[i] = nums1[i] + 1;
+        }
+
+        return true;
+    }
+}
