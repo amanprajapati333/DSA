c3b5b9978629605161b8c63356af93ce37ddaa98
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-09 13:37:46 +0530

    Time: 5 ms (20.16%) | Memory: 268.6 MB (22.43%) - LeetSync

--- LeetcodeProblems/189-rotate-array/rotate-array.java
+++ LeetcodeProblems/189-rotate-array/rotate-array.java
@@ -3,6 +3,29 @@ class Solution {
         int n = nums.length;
         k = k % n;
 
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
+
+/*
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
         int[] temp = new int[k];
 
 
@@ -23,3 +46,4 @@ class Solution {
         }
     }
 }
+*/
