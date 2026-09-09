55d866d2ad7fa762a07a9a0233f5c3e355cde16c
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-09 11:58:15 +0530

    Time: 1 ms (99.36%) | Memory: 42.2 MB (97.44%) - LeetSync

--- LeetcodeProblems/4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ LeetcodeProblems/4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public long countCommas(long n) {
-        long ans = 0;
+        if (n <= 999) return 0;
+
+        long totalCommas = 0;
         long start = 1000;
-        long commas = 1;
 
         while (start <= n) {
-            long end = Math.min(n, start * 1000 - 1);
-
-            ans += (end - start + 1) * commas;
-
+            totalCommas += n - start + 1;
             start *= 1000;
-            commas++;
         }
 
-        return ans;
+        return totalCommas;
     }
 }
