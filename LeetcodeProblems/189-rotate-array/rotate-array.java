3deac6c649450575fc9cc838758f10b3e8cd4b07
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-09 13:34:59 +0530

    Time: 4 ms (40.55%) | Memory: 265.8 MB (37.59%) - LeetSync

---
+++ LeetcodeProblems/189-rotate-array/rotate-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[k];
+
+
+        int j = 0;
+        for (int i = n - k; i < n; i++) {
+            temp[j] = nums[i];
+            j++;
+        }
+
+        // Shift remaining elements to the right
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+
+        // Put last k elements at the beginning
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
