78bce843628270d1181745f2a8c5dcc9a27ceb42
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-11 13:32:25 +0530

    Time: 3 ms (94.11%) | Memory: 46.4 MB (41.78%) - LeetSync

---
+++ LeetcodeProblems/242-valid-anagram/valid-anagram.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //bruth force
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        return Arrays.equals(s1, s2);
+
+
+         /*
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+
+        return true;
+    }
+}*/
+    }
+}
+   
