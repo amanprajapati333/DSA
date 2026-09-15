e05dde726135b7428a1aa4ba5cdf58ef38ad5743
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-16 01:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (39.74%) - LeetSync

--- LeetcodeProblems/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ LeetcodeProblems/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,4 +1,49 @@
+
 class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+          
+            if ((mid == 0 || nums[mid - 1] != nums[mid]) &&
+                (mid == nums.length - 1 || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+
+            if (mid % 2 == 0) {
+
+                if (nums[mid] == nums[mid + 1]) {
+                  
+                    start = mid + 2;
+                } else {
+                   
+                    end = mid - 1;
+                }
+
+            } else {
+
+                if (nums[mid] == nums[mid - 1]) {
+                   
+                    start = mid + 1;
+                } else {
+                    
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+
+
+/*class Solution {
     public int singleNonDuplicate(int[] nums) {
         int left=0;
         int right=nums.length-1;
