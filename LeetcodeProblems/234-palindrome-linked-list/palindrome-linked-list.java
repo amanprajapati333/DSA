1f358b011454f9458a75112c1517dcdd53bc4475
Aman Prajapati <167050491+amanprajapati333@users.noreply.github.com>
2026-09-21 00:30:57 +0530

    Time: 3 ms (99.81%) | Memory: 94.3 MB (79.45%) - LeetSync

--- LeetcodeProblems/234-palindrome-linked-list/palindrome-linked-list.java
+++ LeetcodeProblems/234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,42 +10,37 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
+        if(head==null || head.next==null){
             return true;
         }
-
-        // Find middle
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
+        //reverse second Half 
 
-        // Reverse second half
-        ListNode prev = null;
-        ListNode curr = slow;
-
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        ListNode curr=slow;
+        ListNode prev=null;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
+        //Compare 
+        ListNode first=head;
+        ListNode second=prev;
 
-        // Compare both halves
-        ListNode first = head;
-        ListNode second = prev;
-
-        while (second != null) {
-            if (first.val != second.val) {
+        while(second!=null){
+            if(first.val!=second.val){
                 return false;
             }
-            first = first.next;
-            second = second.next;
+            first=first.next;
+            second=second.next;
         }
 
-        return true;
+       return true;
     }
 }
